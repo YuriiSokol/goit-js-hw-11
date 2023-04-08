32b8e315809aeac5fda5b9ca9ba13b94541e1a80
YuriiSokol <yura357117@gmail.com>
2023-04-08 15:09:40 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+const userKey = `35198946-b1cb389529bf0ab91bbbb016e`;
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { throttle } from 'throttle-debounce';
